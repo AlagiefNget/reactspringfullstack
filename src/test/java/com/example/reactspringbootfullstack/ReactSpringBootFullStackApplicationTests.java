99ec7cd761d808ed53c0fa7ee06b044708a6de6a
Alagie F. Nget <Alagie10@gmail.com>
2022-08-20 19:50:59 -0500

    Commit for test file update v1

--- src/test/java/com/example/reactspringbootfullstack/ReactSpringBootFullStackApplicationTests.java
+++ src/test/java/com/example/reactspringbootfullstack/ReactSpringBootFullStackApplicationTests.java
@@ -9,7 +9,7 @@ class ReactSpringBootFullStackApplicationTests {
 
     @Test
     void contextLoads() {
-        Assertions.fail("Oooops test failed");
+//        Assertions.fail("Oooops test failed");
     }
 
 }
